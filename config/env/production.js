1303ee15027d01d1c071943659259cf5e541e067
siddTalekar <sidd.tale@gmail.com>
2017-03-10 19:04:11 +0530

    env

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 89,
-  // realHost: "http://wohlig.io",
+  realHost: "http://antimatter.tingdom.in",
 
 
 
